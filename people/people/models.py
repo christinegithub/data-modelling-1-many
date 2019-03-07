35f9f9c42c6bf617de36a62c958885232c6e05c2
christinegithub <christine13@gmail.com>
2019-03-07 16:20:27 -0500

    people project complete

--- people/people/models.py
+++ people/people/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
-class Person(models.Model):
-    name = models.CharField(max_length = 255)
-    age = models.IntegerField()
-    residence = models.ForeignKey(Residence, on_delete=models.CASCADE, related_name='person')
-
-class Residence(models.Model):
-    address = models.TextField()
-    year_built = models.IntegerField()
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='residence')
-
-class City(models.Model):
+class Country(models.Model):
     name = models.CharField(max_length = 255)
     year_founded = models.IntegerField()
-    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='city')
+    national_animal = models.CharField(max_length = 255)
 
 class Province(models.Model):
     name = models.CharField(max_length = 255)
     year_founded = models.IntegerField()
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='province')
 
-class Country(models.Model):
+class City(models.Model):
     name = models.CharField(max_length = 255)
     year_founded = models.IntegerField()
-    national_animal = models.CharField(max_length 255)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='city')
+
+class Residence(models.Model):
+    address = models.TextField()
+    year_built = models.IntegerField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='residence')
+    
+class Person(models.Model):
+    name = models.CharField(max_length = 255)
+    age = models.IntegerField()
+    residence = models.ForeignKey(Residence, on_delete=models.CASCADE, related_name='person')
